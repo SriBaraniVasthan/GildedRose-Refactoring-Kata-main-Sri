977465740bc3acc687a7685fd79493e5e02134e1
Sri Barani Vasthan Varadharajan <barani.vatsan@gmail.com>
2024-05-13 15:48:23 +0100

    Refactoring current implementation: Created calculateQuality method to add range checks for quality computations

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -17,53 +17,49 @@ class GildedRose {
     }
 
 	private void updateQualityOfItem(Item item) {
-		if (!item.name.equals(AGED_BRIE)
-		        && !item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT)) {
-		    if (item.quality > 0) {
-		        if (!item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
-		            item.quality = item.quality - 1;
-		        }
-		    }
+		if (!item.name.equals(AGED_BRIE) && !item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT)) {
+
+			if (!item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
+				item.quality = item.quality - 1;
+			}
 		} else {
-		    if (item.quality < 50) {
-		        item.quality = item.quality + 1;
+			calculateQuality(item, 1);
 
-		        if (item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT)) {
-		            if (item.sellIn < 11) {
-		                if (item.quality < 50) {
-		                    item.quality = item.quality + 1;
-		                }
-		            }
+			if (item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT)) {
+				if (item.sellIn < 11) {
+					calculateQuality(item, 1);
+				}
 
-		            if (item.sellIn < 6) {
-		                if (item.quality < 50) {
-		                    item.quality = item.quality + 1;
-		                }
-		            }
-		        }
-		    }
+				if (item.sellIn < 6) {
+					calculateQuality(item, 1);
+				}
+			}
 		}
 
 		if (!item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
-		    item.sellIn = item.sellIn - 1;
+			item.sellIn = item.sellIn - 1;
 		}
 
 		if (item.sellIn < 0) {
-		    if (!item.name.equals(AGED_BRIE)) {
-		        if (!item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT)) {
-		            if (item.quality > 0) {
-		                if (!item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
-		                    item.quality = item.quality - 1;
-		                }
-		            }
-		        } else {
-		            item.quality = item.quality - item.quality;
-		        }
-		    } else {
-		        if (item.quality < 50) {
-		            item.quality = item.quality + 1;
-		        }
-		    }
+			if (!item.name.equals(AGED_BRIE)) {
+				if (!item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT)) {
+					if (!item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
+						item.quality = item.quality - 1;
+					}
+				} else {
+					item.quality = item.quality - item.quality;
+				}
+			} else {
+				calculateQuality(item, 1);
+			}
+		}
+	}
+
+	private void calculateQuality(Item item, int changeInQuality) {
+		int updatedQuality = item.quality + changeInQuality;
+		boolean inValidRange = updatedQuality <= 50 && updatedQuality >= 0;
+		if (inValidRange) {
+			item.quality = updatedQuality;
 		}
 	}
 }
