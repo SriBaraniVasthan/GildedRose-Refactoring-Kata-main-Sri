0fca9d15b81f66819be7db64e9ab39d04578a779
Sri Barani Vasthan Varadharajan <barani.vatsan@gmail.com>
2024-05-13 15:08:02 +0100

    New requirements with conjured items. Adding test cases for  ""Conjured" items degrade in Quality twice as fast as normal items"

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -83,6 +83,20 @@ class GildedRoseTest {
 		assertItemEquals(app.getItems()[0], new Item("Sulfuras, Hand of Ragnaros", 100, 100));
 	}
 	
+	@Test
+	public void item_conjured_decreasesInQuality_twiceTheSpeed() {
+		GildedRose app = new GildedRose(new Item("Conjured Mana Cake", 3, 6));
+		app.updateQuality();
+		assertItemEquals(app.getItems()[0], new Item("Conjured Mana Cake", 2, 4));
+	}
+
+	@Test
+	public void item_conjured_decreasesInQuality_twiceTheSpeed_alsoWhenSellInExpired() {
+		GildedRose app = new GildedRose(new Item("Conjured Mana Cake", 0, 6));
+		app.updateQuality();
+		assertItemEquals(app.getItems()[0], new Item("Conjured Mana Cake", -1, 2));
+	}
+	
 	public static void assertItemEquals(Item actual, Item expected) {
 		assertEquals(expected.name, actual.name);
 		assertEquals(expected.quality, actual.quality);
