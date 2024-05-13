c90d7a8334225519b9124cd8491524fdf31f5f03
Sri Barani Vasthan Varadharajan <barani.vatsan@gmail.com>
2024-05-13 15:00:04 +0100

    Added Unit test case for "Aged Brie" actually increases in Quality the older it gets

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -27,6 +27,27 @@ class GildedRoseTest {
 		assertItemEquals(app.getItems()[0], new Item("foo", -1, 8));
 	}
 
+	@Test
+	public void item_AgedBrie_increasesInQuality() {
+		GildedRose app = new GildedRose(new Item("Aged Brie", 2, 2));
+		app.updateQuality();
+		assertItemEquals(app.getItems()[0], new Item("Aged Brie", 1, 3));
+	}
+
+	@Test
+	public void item_AgedBrie_increasesInQuality_DoublesWhenOff() {
+		GildedRose app = new GildedRose(new Item("Aged Brie", 0, 2));
+		app.updateQuality();
+		assertItemEquals(app.getItems()[0], new Item("Aged Brie", -1, 4));
+	}
+
+	@Test
+	public void item_AgedBrie_cannotGoOver50Quality() {
+		GildedRose app = new GildedRose(new Item("Aged Brie", 2, 50));
+		app.updateQuality();
+		assertItemEquals(app.getItems()[0], new Item("Aged Brie", 1, 50));
+	}
+
 	public static void assertItemEquals(Item actual, Item expected) {
 		assertEquals(expected.name, actual.name);
 		assertEquals(expected.quality, actual.quality);
