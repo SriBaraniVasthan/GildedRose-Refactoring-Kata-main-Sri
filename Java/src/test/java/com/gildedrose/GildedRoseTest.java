9b1d9a9911474e461df5018126c35388e0f6af68
Sri Barani Vasthan Varadharajan <barani.vatsan@gmail.com>
2024-05-13 14:55:50 +0100

    Added Test cases for 1. "Once the sell by date has passed, Quality degrades twice as fast". 2."The Quality of an item is never negative and never more than 50."

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -6,12 +6,31 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
 
-    @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
-    }
+	@Test
+	public void sellInDateDecreases_butQualityCannotBeNegative() {
+		GildedRose app = new GildedRose(new Item("foo", 0, 0));
+		app.updateQuality();
+		assertItemEquals(app.getItems()[0], new Item("foo", -1, 0));
+	}
+
+	@Test
+	public void qualityDecreases() {
+		GildedRose app = new GildedRose(new Item("foo", 10, 10));
+		app.updateQuality();
+		assertItemEquals(app.getItems()[0], new Item("foo", 9, 9));
+	}
+
+	@Test
+	public void qualityDecreasesFasterAfterSellInDateExpired() {
+		GildedRose app = new GildedRose(new Item("foo", 0, 10));
+		app.updateQuality();
+		assertItemEquals(app.getItems()[0], new Item("foo", -1, 8));
+	}
+
+	public static void assertItemEquals(Item actual, Item expected) {
+		assertEquals(expected.name, actual.name);
+		assertEquals(expected.quality, actual.quality);
+		assertEquals(expected.sellIn, actual.sellIn);
+	}
 
 }
